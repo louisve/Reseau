ec142e45d646460d0ed20d7325127b3e38aeaf55
DramatikzZ <98904133+DramatikzZ@users.noreply.github.com>
2023-03-01 15:13:21 +0100

    make + pixel

--- main.c
+++ main.c
@@ -1,18 +1,4 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <errno.h>
-#include <unistd.h> 
-#include <arpa/inet.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <sys/time.h>
- 
-#define TRUE   1
-#define FALSE  0
-#define PORT 5000
-#define LG_MESSAGE 256
+#include "pixel.h"
 
 int main(int argc , char *argv[])
 {
